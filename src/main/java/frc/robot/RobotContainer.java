796774f4610a6885069c7c5d2eb3556f0327c73f
napro1299 <nick.b.amato@gmail.com>
2022-01-13 20:58:05 -0500

    RobotContainer updated with controlable robot code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,20 +1,32 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.DriveCommand;
+import frc.robot.commands.DriveTeleopCommand;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.Swerve;
 
 public class RobotContainer {
-    private final DriveSubsystem driveSubsystem = new DriveSubsystem();
+
+    private final Swerve swerve = new Swerve();
+
+    private final PS4Controller driverController = new PS4Controller(0);
 
     public RobotContainer() {
         configureButtonBindings();
-        initCommands();
+        configureDefaultCommands();
     }
 
-    private void initCommands() {
-        driveSubsystem.setDefaultCommand(new DriveCommand(driveSubsystem, 0.4));
+    private void configureDefaultCommands() {
+        swerve.setDefaultCommand(
+                new DriveTeleopCommand(
+                        driverController::getLeftX,
+                        driverController::getLeftY,
+                        () -> -driverController.getRightX(),
+                        true,
+                        swerve));
     }
 
     private void configureButtonBindings() {}
