4b44591be6c092ced0256d1e5ff7718d9c3acd48
Alex Odom <alexjodom@gmail.com>
2022-01-19 12:32:51 -0500

    Added Set Speed Feed with Feedforward calculation

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -11,11 +11,11 @@ import com.ctre.phoenix.motorcontrol.can.SlotConfiguration;
 import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.sensors.*;
+
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import frc.robot.util.CTREModuleState;
-//import io.github.oblarg.oblog.Loggable;
-//import io.github.oblarg.oblog.annotations.Log;
 
 /** A hardware wrapper class for a swerve module that uses Falcon500s. */
 public class SwerveModule /*implements Loggable*/ {
@@ -25,11 +25,16 @@ public class SwerveModule /*implements Loggable*/ {
     private static final double STEER_GEAR_RATIO = 12.8; // : 1
     private static final int VELOCITY_PID_SLOT = 0;
     private static final int MOTION_MAGIC_PID_SLOT = 1;
-    private static final double STEERING_KS = 0.075;
 
     private static final TalonFXConfiguration WHEEL_MOTOR_CONFIG = new TalonFXConfiguration();
     private static final TalonFXConfiguration STEERING_MOTOR_CONFIG = new TalonFXConfiguration();
     private static final CANCoderConfiguration CONFIGURATION = new CANCoderConfiguration();
+
+    private final SimpleMotorFeedforward wheelFeedforward = new SimpleMotorFeedforward(
+        0.0,
+        0.0,
+        0.0
+    );
 
     static {
         // Wheel Motor Current Limiting
@@ -45,6 +50,8 @@ public class SwerveModule /*implements Loggable*/ {
 
         var wheelPID = new SlotConfiguration();
         wheelPID.kP = 0.08; // 0.012
+
+        // TODO: Remove once velocity feedforward is calculated
         wheelPID.kF = 0.05;
 
         WHEEL_MOTOR_CONFIG.slot0 = wheelPID;
@@ -173,9 +180,7 @@ public class SwerveModule /*implements Loggable*/ {
         // Custom optimize command, since default WPILib optimize assumes continuous controller which
         // CTRE is not
         state = CTREModuleState.optimize(state, getState().angle);
-        final double velocity =
-                MPSToFalcon(state.speedMetersPerSecond, Math.PI * WHEEL_DIAMETER, WHEEL_GEAR_RATIO);
-        wheelMotor.set(ControlMode.Velocity, velocity);
+        setSpeed(state.speedMetersPerSecond);
 
         // The speed of the wheel is really low (less than 0.01% Max speed) don't
         // steer. This prevents jittering
@@ -187,9 +192,16 @@ public class SwerveModule /*implements Loggable*/ {
         lastAngle = angle;
     }
 
-    /** Configure the name of the log. Used by the Oblog logging system. */
-    public String configureLogName() {
-        return moduleName;
+    /**
+     * Set the speed of the wheel.
+     * 
+     * @param wheelVelocityMPS Speed of the wheel in meters per second
+     */
+    public void setSpeed(double wheelVelocityMPS) {
+        final double velocity =
+                MPSToFalcon(wheelVelocityMPS, Math.PI * WHEEL_DIAMETER, WHEEL_GEAR_RATIO);
+        wheelMotor.set(ControlMode.Velocity, velocity,
+            DemandType.ArbitraryFeedForward, wheelFeedforward.calculate(wheelVelocityMPS));
     }
 
     /** Resets the steering motor's internal encoder to the value of the absolute encoder. * */
