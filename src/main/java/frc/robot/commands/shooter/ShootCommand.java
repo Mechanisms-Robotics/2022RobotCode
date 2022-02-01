e6786a297efd63eeae79bfde4bf963384d8caf54
JonathanREmery <JonathanREmery@gmail.com>
2022-02-01 16:33:24 -0500

    Fixed bug where shoot command would would wait for SpinupCommand to end which will never happen.

--- src/main/java/frc/robot/commands/shooter/ShootCommand.java
+++ src/main/java/frc/robot/commands/shooter/ShootCommand.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.shooter;
 
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.accelerator.AcceleratorCommand;
 import frc.robot.commands.feeder.FeederCommand;
@@ -7,7 +8,7 @@ import frc.robot.subsystems.Accelerator;
 import frc.robot.subsystems.Feeder;
 import frc.robot.subsystems.Shooter;
 
-public class ShootCommand extends SequentialCommandGroup {
+public class ShootCommand extends ParallelCommandGroup {
   public final Feeder feeder;
   public final Accelerator accelerator;
   public final Shooter shooter;
