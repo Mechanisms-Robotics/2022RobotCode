dba6ab11ea7bfcb2979cc0c11c305a25af0f10c1
napro1299 <nick.b.amato@gmail.com>
2022-03-19 14:51:01 -0400

    Intake retract functionality

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -7,9 +7,13 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.StatusFrame;
 import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+import com.ctre.phoenix.motorcontrol.can.SlotConfiguration;
 import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.util.Units;
+
+import java.util.concurrent.locks.ReentrantLock;
 
 /** This class contains all the code responsible for the behaviour of the intake class */
 public class Intake extends SubsystemBase {
@@ -18,8 +22,14 @@ public class Intake extends SubsystemBase {
   private static final double INTAKE_SPEED = 0.7; // percent
   private static final double OUTTAKE_SPEED = -0.25; // percent
 
+  private static final int INTAKE_RETRACTED_POSITION = 0; // TODO: get values
+  private static final int INTAKE_DEPLOYED_POSITION = 0;
+
+  private static final int INTAKE_RETRACTED_GEAR_RATIO = 88; // TODO: validate gear ratio
+
   // Intake motor configuration
   private static final TalonFXConfiguration INTAKE_MOTOR_CONFIG = new TalonFXConfiguration();
+  private static final TalonFXConfiguration INTAKE_RETRACT_MOTOR_CONFIG;
 
   // Configure the intake current limit
   static {
@@ -29,21 +39,37 @@ public class Intake extends SubsystemBase {
     intakeCurrentLimit.triggerThresholdTime = 0.25; // sec
     intakeCurrentLimit.enable = true;
     INTAKE_MOTOR_CONFIG.supplyCurrLimit = intakeCurrentLimit;
+    INTAKE_RETRACT_MOTOR_CONFIG = INTAKE_MOTOR_CONFIG;
+
+    final SlotConfiguration intakeRetractMMPID = new SlotConfiguration();
+    intakeRetractMMPID.kP = 0.1;
+    INTAKE_RETRACT_MOTOR_CONFIG.slot1 = intakeRetractMMPID;
+    INTAKE_RETRACT_MOTOR_CONFIG.motionCruiseVelocity = 0.0; // TODO: Find values
+    INTAKE_RETRACT_MOTOR_CONFIG.motionAcceleration = 0.0;
+    INTAKE_RETRACT_MOTOR_CONFIG.motionCurveStrength = 0;
   }
+
+  private boolean deployed = false;
 
   // Intake motor
   private final WPI_TalonFX intakeMotor = new WPI_TalonFX(20);
+  private final WPI_TalonFX intakeRetractMotor = new WPI_TalonFX(21); // TODO: rename motor
 
   /** Constructs an Intake */
   public Intake() {
     // Configure intake motor
     intakeMotor.configAllSettings(INTAKE_MOTOR_CONFIG, startupCanTimeout);
+    intakeRetractMotor.configAllSettings(INTAKE_MOTOR_CONFIG, startupCanTimeout);
     intakeMotor.setInverted(TalonFXInvertType.Clockwise);
+    intakeRetractMotor.setInverted(TalonFXInvertType.Clockwise);
     intakeMotor.setNeutralMode(NeutralMode.Coast);
+    intakeRetractMotor.setNeutralMode(NeutralMode.Coast);
 
     // CAN bus utilization optimization
-    intakeMotor.setStatusFramePeriod(StatusFrame.Status_1_General, 255);
     intakeMotor.setStatusFramePeriod(StatusFrame.Status_2_Feedback0, 255);
+    intakeMotor.setStatusFramePeriod(StatusFrame.Status_1_General, 255);
+    intakeRetractMotor.setStatusFramePeriod(StatusFrame.Status_2_Feedback0, 255);
+    intakeRetractMotor.setStatusFramePeriod(StatusFrame.Status_1_General, 255);
   }
 
   /**
@@ -53,6 +79,18 @@ public class Intake extends SubsystemBase {
    */
   private void setOpenLoop(double percentOutput) {
     intakeMotor.set(ControlMode.PercentOutput, percentOutput);
+  }
+
+  public void deploy() {
+    if (!deployed) {
+      intakeRetractMotor.set(ControlMode.MotionMagic, INTAKE_DEPLOYED_POSITION);
+    }
+  }
+
+  public void retract() {
+    if (deployed) {
+      intakeRetractMotor.set(ControlMode.MotionMagic, INTAKE_RETRACTED_POSITION);
+    }
   }
 
   /** Runs the intake at INTAKE_SPEED */
